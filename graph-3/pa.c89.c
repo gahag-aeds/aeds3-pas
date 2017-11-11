cf1b806ea4dd73f7e0cff5683fd7c184cbec8ead
gahag <gabriel.s.b@live.com>
2017-11-11 01:46:35 -0300

    Graph-3.

---
+++ graph-3/pa.c89.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+unsigned short order;
+short graph[1000][1000];
+
+struct {
+  unsigned short visited;
+  unsigned short time;
+} memo[1000];
+
+
+unsigned short dfs(unsigned short v, unsigned short color) {
+  if (memo[v].visited == color)
+    return 0;
+  
+  memo[v].visited = color;
+  memo[v].time = 0;
+  
+  unsigned short i;
+  for (i = 0; i < 1000; i++) {
+    if (graph[v][i] == -1)
+      break;
+    
+    unsigned short deptime = dfs(graph[v][i], color);
+    
+    if (deptime == 0)
+      return 0;
+    
+    memo[v].time = memo[v].time > deptime ? memo[v].time : deptime;
+  }
+  
+  memo[v].time++;
+  
+  return memo[v].time;
+}
+
+
+int main() {
+  while (scanf("%hu", &order) == 1) {
+    memset(graph, -1, sizeof(graph));
+    memset(memo, 0, sizeof(memo));
+    
+    unsigned short i;
+    for (i = 0; i < order; i++) {
+      unsigned short n;
+      scanf("%hu", &n);
+      
+      unsigned short j;
+      for (j = 0; j < n; j++) {
+        unsigned short d;
+        scanf("%hu", &d);
+        graph[i][j] = d - 1;
+      }
+    }
+    
+    unsigned short max = 0;
+    for (i = 0; i < order; i++) {
+      unsigned short time = memo[i].visited ? memo[i].time : dfs(i, i + 1);
+      
+      if (time == 0) {
+        max = 0;
+        break;
+      }
+      
+      max = max > time ? max : time;
+    }
+    
+    printf("%d\n", max == 0 ? -1 : (int) max);
+  }
+  
+  return 0;
+}
